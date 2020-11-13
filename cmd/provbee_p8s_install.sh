0f351fde09fe39b803f0f32ce95ddfb3ea64bcb5
SKYul <folimworld@gmail.com>
2020-11-13 17:59:51 +0900

    Create provbee_p8s_install.sh
    p8s install script

---
+++ cmd/provbee_p8s_install.sh
@@ -0,0 +1,15 @@
+JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
+####################################################################
+PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
+TOBSYAML=$(echo ${JSON_TASK_PARAMS} | jq '.tobs_yaml')
+GRAFANA_PW=$(echo ${JSON_TASK_PARAMS} | jq -r '.grafana_credential')
+
+#helm test
+provbeetmp="NexClipper.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
+ssh provbee-service "echo $TOBSYAML > /tmp/$provbeetmp.base64"
+ssh provbee-service busybee tobs install $PROVNS $provbeetmp
+#ssh provbee-service kubectl patch service -n $PROVNS nc-grafana -p '{\"spec\":{\"type\":\"NodePort\"}}'
+######TEST INFO
+grafanapw=`ssh provbee-service busybee tobs passwd $PROVNS $GRAFANA_PW`
+grafanainfo=`ssh provbee-service busybee nodesearch nc-grafana`
+TASK_RESULT=$(echo "{}" | jq '. += {"GRAFANA_URL": "'"$grafanainfo"'", "GRAFANA_PW_CHANGE": "'"$grafanapw"'"}')
