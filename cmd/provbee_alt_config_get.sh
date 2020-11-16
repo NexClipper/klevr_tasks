5384ed7181c327c34c71b35879d8b3f511997ae8
Chopa Jang <ys3gods@gmail.com>
2020-11-16 20:19:42 +0900

    Update provbee_alt_config_get.sh

--- cmd/provbee_alt_config_get.sh
+++ cmd/provbee_alt_config_get.sh
@@ -1,4 +1,4 @@
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 alertmanager_config=`ssh provbee-service busybee p8s cm $PROVNS get alertmanager`
-TASK_RESULT=$(echo "{}" | jq '. += {"Alertmanager_config_get": "'"$alertmanager_config"'"}')
+TASK_RESULT=$(echo "{}" | jq '. += {"AM_CONFIG_YAML": "'"$alertmanager_config"'"}')
