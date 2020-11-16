a78d943e7b5939468a2ddbbfeb44c5a977befcf0
Chopa Jang <ys3gods@gmail.com>
2020-11-16 23:07:05 +0900

    Update provbee_p8s_config_apply.sh

--- cmd/provbee_p8s_config_apply.sh
+++ cmd/provbee_p8s_config_apply.sh
@@ -4,4 +4,4 @@ PROMCONFALY=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_config_yaml')
 provbeetmp="Nex-prom_config.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
 ssh provbee-service "echo $PROMCONFALY > /tmp/$provbeetmp.base64"
 prometheus_config_apply=`ssh provbee-service busybee p8s cm $PROVNS apply prometheus $provbeetmp`
-TASK_RESULT=$(echo "{}" | jq '. += {"Prometheus_config_apply": "'"$prometheus_config_apply"'"}')
+TASK_RESULT=$(echo "{}" | jq '. += {"P8S_CONFIG_YAML": "'"$prometheus_config_apply"'"}')
