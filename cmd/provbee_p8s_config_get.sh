66eb29037b78708a1c638b10886a8882a0225a33
SKYul <folimworld@gmail.com>
2020-11-16 19:25:50 +0900

    Update provbee_p8s_config_get.sh

--- cmd/provbee_p8s_config_get.sh
+++ cmd/provbee_p8s_config_get.sh
@@ -1,5 +1,4 @@
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 prometheus_config=`ssh provbee-service busybee p8s cm $PROVNS get prometheus`
-alertmanager_config=`ssh provbee-service busybee p8s cm $PROVNS get alertmanager`
-TASK_RESULT=$(echo "{}" | jq '. += {"Prometheus_config_get": "'"$prometheus_config"'", "Alertmanager_config_get": "'"$alertmanager_config"'"}')
+TASK_RESULT=$(echo "{}" | jq '. += {"Prometheus_config_get": "'"$prometheus_config"'"}')
