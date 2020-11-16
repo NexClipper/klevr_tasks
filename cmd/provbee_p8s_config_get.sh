52d2f78ee7e55e8e285fd7f8e3aa4390b7be3104
Chopa Jang <ys3gods@gmail.com>
2020-11-16 20:16:53 +0900

    Update provbee_p8s_config_get.sh

--- cmd/provbee_p8s_config_get.sh
+++ cmd/provbee_p8s_config_get.sh
@@ -1,4 +1,4 @@
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 prometheus_config=`ssh provbee-service busybee p8s cm $PROVNS get prometheus`
-TASK_RESULT=$(echo "{}" | jq '. += {"Prometheus_config_get": "'"$prometheus_config"'"}')
+TASK_RESULT=$(echo "{}" | jq '. += {"P8S_CONFIG_YAML": "'"$prometheus_config"'"}')
