a74138befcb60af7fae84afb338868aa81190293
SKYul <folimworld@gmail.com>
2020-12-16 15:50:16 +0900

    Update provbee_gf_expose.sh

--- cmd/provbee_gf_expose.sh
+++ cmd/provbee_gf_expose.sh
@@ -1,6 +1,10 @@
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
+####### JSON
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
+GRAFANA_PW=$(echo ${JSON_TASK_PARAMS} | jq -r '.grafana_credential')
+####### RUN
 ssh provbee-service kubectl patch service -n $PROVNS nc-grafana -p '{\"spec\":{\"type\":\"NodePort\"}}'
+grafanapw=`ssh provbee-service busybee tobs passwd $PROVNS $GRAFANA_PW`
 grafanainfo=`ssh provbee-service busybee nodesearch nc-grafana`
-#TASK_RESULT=$(echo "{}" | jq '. += {"GRAFANA_URL": "'"$grafanainfo"'"}')
-TASK_RESULT=$(echo "{\"GRAFANA_URL\": \"$grafanainfo\"}")
+######## INFO
+TASK_RESULT=$(echo "{}" | jq '. += {"GRAFANA_URL": "'"$grafanainfo"'", "GRAFANA_PW_CHANGE": "'"$grafanapw"'"}')
