aa614e8e5d4156cd4d28ee53a2e96e71e1780455
SKYul <folimworld@gmail.com>
2020-12-16 16:16:01 +0900

    Update provbee_p8s_install.sh

--- cmd/provbee_p8s_install.sh
+++ cmd/provbee_p8s_install.sh
@@ -2,12 +2,10 @@ JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 ####################################################################
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 TOBSYAML=$(echo ${JSON_TASK_PARAMS} | jq '.tobs_yaml')
-GRAFANA_PW=$(echo ${JSON_TASK_PARAMS} | jq -r '.grafana_credential')
 
 echo "JSON_TASK_PARAMS=${JSON_TASK_PARAMS}\n" > /tmp/klevr_debug
 echo "PROVNS=${PROVNS}\n" >> /tmp/klevr_debug
 echo "TOBSYAML=${TOBSYAML}\n" >> /tmp/klevr_debug
-echo "GRAFANA_PW=${GRAFANA_PW}\n\n\n" >> /tmp/klevr_debug
 
 #helm test
 provbeetmp="NexClipper.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
@@ -18,10 +16,19 @@ cat /tmp/$provbeetmp.base64 >> /tmp/klevr_debug
 echo "\n\n" >> /tmp/klevr_debug
 
 ssh provbee-service busybee tobs install $PROVNS $provbeetmp
-#ssh provbee-service kubectl patch service -n $PROVNS nc-grafana -p '{\"spec\":{\"type\":\"NodePort\"}}'
+##########IN
+#p8sinstchk=`ssh provbee-service busybee tobs install_chk`
+#TASK_RESULT=$(echo "{}" | jq '. += {"P8S_INSTALL": "'"$p8sinstchk"'"}')
+##########IN
+
+############DELLLLLLLLLLLLLL
 ######TEST INFO
+############DELLLLLLLLLLLLLL 
+GRAFANA_PW=$(echo ${JSON_TASK_PARAMS} | jq -r '.grafana_credential')
+echo "GRAFANA_PW=${GRAFANA_PW}\n\n\n" >> /tmp/klevr_debug
 grafanapw=`ssh provbee-service busybee tobs passwd $PROVNS $GRAFANA_PW`
 grafanainfo=`ssh provbee-service busybee nodesearch nc-grafana`
 TASK_RESULT=$(echo "{}" | jq '. += {"GRAFANA_URL": "'"$grafanainfo"'", "GRAFANA_PW_CHANGE": "'"$grafanapw"'"}')
+############DELLLLLLLLLLLLLL
 
 echo TASK_RESULT=${TASK_RESULT} >> /tmp/klevr_debug
