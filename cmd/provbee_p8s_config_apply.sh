aa10a33bcb93590dce1307f50359070025cd2470
SKYul <folimworld@gmail.com>
2020-12-17 15:33:57 +0900

    Update provbee_p8s_config_apply.sh

--- cmd/provbee_p8s_config_apply.sh
+++ cmd/provbee_p8s_config_apply.sh
@@ -2,9 +2,9 @@
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 PROMCONFALY=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_config_yaml')
-provbeetmp="Nex-prom_config.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
 
 ##### RUN
+provbeetmp="Nex-prom_config.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
 ssh provbee-service "echo $PROMCONFALY > /tmp/$provbeetmp.base64"
 prometheus_config_apply=`ssh provbee-service busybee p8s cm $PROVNS apply prometheus $provbeetmp`
 
