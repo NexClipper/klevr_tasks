c264104ef53fe00fba1e4ea8aa8f247e38d95348
SKYul <folimworld@gmail.com>
2020-12-17 14:08:54 +0900

    Update provbee_p8s_config_test.sh

--- cmd/provbee_p8s_config_test.sh
+++ cmd/provbee_p8s_config_test.sh
@@ -8,8 +8,7 @@ PROMCONFALY=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_config_yaml')
 ###### RUN
 provbeetmp="Nex-prom_config_test.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
 ssh provbee-service "echo $PROMCONFALY > /tmp/$provbeetmp.base64"
-ssh provbee-service busybee p8s cm $PROVNS test prometheus $provbeetmp
-#prometheus_config_test=`ssh provbee-service busybee p8s cm $PROVNS get prometheus`
+prometheus_config_test=`ssh provbee-service busybee p8s cm $PROVNS test prometheus $provbeetmp`
 
 ###### INFO
 TASK_RESULT=$(echo "{}" | jq '. += {"P8S_CONFIG_YAML_TEST": "'"$prometheus_config_test"'"}')
