83f43087199b351bff50cc5e8d6098c383e88273
SKYul <folimworld@gmail.com>
2020-12-22 18:12:20 +0900

    Update provbee_alt_config_test.sh

--- cmd/provbee_alt_config_test.sh
+++ cmd/provbee_alt_config_test.sh
@@ -3,7 +3,7 @@
 ###### JSON
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
-PROMCONFALY=$(echo ${JSON_TASK_PARAMS} | jq -r '.am_config_yaml')
+ALTCONFALY=$(echo ${JSON_TASK_PARAMS} | jq -r '.am_config_yaml')
 
 ###### RUN
 provbeetmp="Nex-alt_config_test.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
