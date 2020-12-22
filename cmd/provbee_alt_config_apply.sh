d39767328afbc6e953a8133bc55060b64b830051
SKYul <folimworld@gmail.com>
2020-12-22 18:13:04 +0900

    Update provbee_alt_config_apply.sh

--- cmd/provbee_alt_config_apply.sh
+++ cmd/provbee_alt_config_apply.sh
@@ -1,7 +1,12 @@
+##### JSON
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 ALTCONFALY=$(echo ${JSON_TASK_PARAMS} | jq -r '.am_config_yaml')
+
+##### RUN
 provbeetmp="Nex-alt_config.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
 ssh provbee-service "echo $ALTCONFALY > /tmp/$provbeetmp.base64"
 alertmanager_config_apply=`ssh provbee-service busybee p8s cm $PROVNS apply alertmanager $provbeetmp`
+
+##### INFO
 TASK_RESULT=$(echo "{}" | jq '. += {"AM_CONFIG_YAML": "'"$alertmanager_config_apply"'"}')
