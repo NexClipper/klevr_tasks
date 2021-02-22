624930a35370ccc03191fa47b6f9c0546bffbe2a
SKYul <folimworld@gmail.com>
2021-02-22 16:43:28 +0900

    global view
    provbee job
    default view : k8s 
    global view : gstatus

--- cmd/provbee_k8s_status.sh
+++ cmd/provbee_k8s_status.sh
@@ -1,4 +1,11 @@
+##### JSON
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
+#GETJOB=$(echo ${JSON_TASK_PARAMS} | jq -r '.provbee_job')
+
+##### RUN
 k8s_status=`ssh provbee-service busybee k8s wow $PROVNS` 
+#k8s_status=`ssh provbee-service busybee $GETJOB wow $PROVNS` 
+
+##### INFO
 TASK_RESULT=$k8s_status
