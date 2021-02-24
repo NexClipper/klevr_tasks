6f7d5e3db23b094b999aa6866abb17c368f2179f
SKYul <folimworld@gmail.com>
2021-02-24 10:42:12 +0900

    Update provbee_p8s_install_global.sh

--- cmd/provbee_p8s_install_global.sh
+++ cmd/provbee_p8s_install_global.sh
@@ -4,8 +4,7 @@ PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 TOBSYAML=$(echo ${JSON_TASK_PARAMS} | jq '.tobs_yaml')
 GLOBALINST=$(echo ${JSON_TASK_PARAMS} | jq '.p8s_install_global')
 
-#OLD
-GRAFANA_PW=$(echo ${JSON_TASK_PARAMS} | jq -r '.grafana_credential')
+
 
 
 echo "JSON_TASK_PARAMS=${JSON_TASK_PARAMS}\n" > /tmp/klevr_debug
@@ -15,8 +14,7 @@ echo "GRAFANA_PW=${GRAFANA_PW}\n\n\n" >> /tmp/klevr_debug
 
 #helm test
 ##### RUN
-if [[ $GLOBALINST == "Y" ]]; then globalview="_GLOBAL";fi
-provbeetmp="NexClipper${globalview}.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
+provbeetmp="NexClipper.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
 echo "provbeetmp=${provbeetmp}\n\n" >> /tmp/klevr_debug
 
 ssh provbee-service "echo $TOBSYAML > /tmp/$provbeetmp.base64"
