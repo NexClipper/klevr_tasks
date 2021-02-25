4f77c34f3a284548700b16d6e419ecab4607b6ff
joel-nexcl <75293076+joel-nexcl@users.noreply.github.com>
2021-02-25 22:04:28 +0900

    Update provbee_p8s_install.sh

--- cmd/provbee_p8s_install.sh
+++ cmd/provbee_p8s_install.sh
@@ -4,8 +4,7 @@ PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 TOBSYAML=$(echo ${JSON_TASK_PARAMS} | jq '.tobs_yaml')
 GLOBALINST=$(echo ${JSON_TASK_PARAMS} | jq '.p8s_install_global')
 
-#OLD
-GRAFANA_PW=$(echo ${JSON_TASK_PARAMS} | jq -r '.grafana_credential')
+
 
 
 echo "JSON_TASK_PARAMS=${JSON_TASK_PARAMS}\n" > /tmp/klevr_debug
@@ -15,25 +14,12 @@ echo "GRAFANA_PW=${GRAFANA_PW}\n\n\n" >> /tmp/klevr_debug
 
 #helm test
 ##### RUN
-if [[ $GLOBALINST == "Y" ]]; then globalview="_GLOBAL";fi
-provbeetmp="NexClipper${globalview}.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
+provbeetmp="NexClipper.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
 echo "provbeetmp=${provbeetmp}\n\n" >> /tmp/klevr_debug
 
-
-################### OLD
 ssh provbee-service "echo $TOBSYAML > /tmp/$provbeetmp.base64"
-ssh provbee-service busybee tobs install $PROVNS $provbeetmp
-ssh provbee-service busybee tobs instpw $PROVNS $GRAFANA_PW
-p8sinstchk=`ssh provbee-service busybee tobs install_chk`
-grafanapw=`ssh provbee-service busybee tobs passwd $PROVNS`
-cat /tmp/$provbeetmp.base64 >> /tmp/klevr_debug
-echo "\n\n" >> /tmp/klevr_debug
+tobs_svc=$(ssh provbee-service busybee tobs install $PROVNS $provbeetmp)
 
-TASK_RESULT=$(echo "{}" | jq '. += {"P8S_INSTALL": "'"$p8sinstchk"'"}')
 
-echo TASK_RESULT=${TASK_RESULT} >> /tmp/klevr_debug
-
-################## NEW
-#ssh provbee-service "echo $TOBSYAML > /tmp/$provbeetmp.base64"
-#tobs_svc=$(ssh provbee-service busybee tobs install $PROVNS $provbeetmp)
-#TASK_RESULT=$(echo "$tobs_svc")
+##### INFO
+TASK_RESULT=$(echo "$tobs_svc")
