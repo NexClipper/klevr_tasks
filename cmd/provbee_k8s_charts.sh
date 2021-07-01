06f3a4c2f30f4d13cef38611af280ff0b67990a4
SKYul <folimworld@gmail.com>
2021-07-01 17:20:05 +0900

    Update provbee_k8s_charts.sh

--- cmd/provbee_k8s_charts.sh
+++ cmd/provbee_k8s_charts.sh
@@ -5,15 +5,19 @@ GET_CMD=$(echo ${JSON_TASK_PARAMS} | jq -r '.charts_cmd')
 GET_APP=$(echo ${JSON_TASK_PARAMS} | jq -r '.charts_app')
 GET_YAML=$(echo ${JSON_TASK_PARAMS} | jq -r '.charts_yaml')
 GET_TYPE=$(echo ${JSON_TASK_PARAMS} | jq -r '.charts_svc_type')
-if [ "$GET_YAML" = "null" ]; then GET_YAML="" ; fi
-if [ "$GET_TYPE" = "null" ]; then GET_TYPE="" ; fi
+
 
 #helm test
 ##### RUN
-GET_TMP="Nex_charts.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
-ssh provbee-service "echo $GET_YAML > /tmp/$GET_TMP.base64"
+if [ "$GET_YAML" = "null" ]; then 
+  GET_YAML=""
+else
+  GET_TMP="Nex_charts.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
+  ssh provbee-service "echo $GET_YAML > /tmp/$GET_TMP.base64"
+fi
+if [ "$GET_TYPE" = "null" ]; then GET_TYPE="" ; fi
+
 GET_JSON=$(ssh provbee-service busybee charts $GET_CMD $GET_NS $GET_APP $GET_TMP $GET_TYPE)
-#tobs_svc=$(ssh provbee-service busybee tobs install $PROVNS $provbeetmp)
 
 
 ##### INFO
