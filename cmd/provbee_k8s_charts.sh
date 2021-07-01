fbf0f1ad65cfd8f2ff5c8c123fa4be46a5062e4e
SKYul <folimworld@gmail.com>
2021-07-01 17:13:30 +0900

    Update provbee_k8s_charts.sh

--- cmd/provbee_k8s_charts.sh
+++ cmd/provbee_k8s_charts.sh
@@ -1,25 +1,20 @@
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 ####################################################################
-#PROVNS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
-#TOBSYAML=$(echo ${JSON_TASK_PARAMS} | jq '.tobs_yaml')
-#GLOBALINST=$(echo ${JSON_TASK_PARAMS} | jq '.p8s_install_global')
-
-
-
-
-#echo "JSON_TASK_PARAMS=${JSON_TASK_PARAMS}\n" > /tmp/klevr_debug
-#echo "PROVNS=${PROVNS}\n" >> /tmp/klevr_debug
-#echo "TOBSYAML=${TOBSYAML}\n" >> /tmp/klevr_debug
-#echo "GRAFANA_PW=${GRAFANA_PW}\n\n\n" >> /tmp/klevr_debug
+GET_NS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
+GET_CMD=$(echo ${JSON_TASK_PARAMS} | jq -r '.charts_cmd')
+GET_APP=$(echo ${JSON_TASK_PARAMS} | jq -r '.charts_app')
+GET_YAML=$(echo ${JSON_TASK_PARAMS} | jq '.charts_yaml')
+GET_TYPE=$(echo ${JSON_TASK_PARAMS} | jq -r '.charts_svc_type')
+if [ "$GET_YAML" = "" ]; then GET_YAML="" ; fi
+if [ "$GET_TYPE" = "" ]; then GET_TYPE="" ; fi
 
 #helm test
 ##### RUN
-#provbeetmp="NexClipper.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
-#echo "provbeetmp=${provbeetmp}\n\n" >> /tmp/klevr_debug
-
-#ssh provbee-service "echo $TOBSYAML > /tmp/$provbeetmp.base64"
+GET_TMP="Nex_charts.$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | sed 1q)"
+ssh provbee-service "echo $GET_YAML > /tmp/$GET_TMP.base64"
+GET_JSON=$(ssh provbee-service busybee charts $GET_CMD $GET_NS $GET_APP $GET_TMP $GET_TYPE)
 #tobs_svc=$(ssh provbee-service busybee tobs install $PROVNS $provbeetmp)
 
 
 ##### INFO
-#TASK_RESULT=$(echo "$tobs_svc")
+TASK_RESULT=$(echo "$GET_JSON")
