605b279d9125e7326d665250ab0d50b81e9671d4
joel83 <75293076+joel-nexcl@users.noreply.github.com>
2021-07-12 16:15:50 +0900

    Update provbee_whoisip.sh

--- cmd/provbee_whoisip.sh
+++ cmd/provbee_whoisip.sh
@@ -2,8 +2,6 @@
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 GETJOB=$(echo ${JSON_TASK_PARAMS} | jq -r '.provbee_job')
 
-#TEST
-if [ "$GETJOB" = "" ]; then GETJOB="whoisip"; fi
 ##### RUN
 whoisip =`ssh provbee-service busybee $GETJOB
 
