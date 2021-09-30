ad0704ea26d9c5354a8fb115d11eaa7a63a5b50c
joel83 <75293076+joel-nexcl@users.noreply.github.com>
2021-09-30 13:23:04 +0900

    Update provbee_metricark_promql.sh

--- cmd/provbee_metricark_promql.sh
+++ cmd/provbee_metricark_promql.sh
@@ -1,7 +1,7 @@
 ##### JSON
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
-GET_NS=$(echo ${JSON_TASK_PARAMS} | jq -r '.p8s_namespace')
 GET_CMD=$(echo ${JSON_TASK_PARAMS} | jq -r '.metricark_cmd')
+GET_PROMQL=$(echo ${JSON_TASK_PARAMS} | jq -r '.metricark_promql')
 GET_ENDPOINT=$(echo ${JSON_TASK_PARAMS} | jq -r '.metricark_endpoint')
 
 
@@ -9,7 +9,7 @@ GET_ENDPOINT=$(echo ${JSON_TASK_PARAMS} | jq -r '.metricark_endpoint')
 if [ "$GET_CMD" = "" ]; then GET_CMD="metricark_promql"; fi
 ##### RUN
 #GET_JSON=`ssh provbee-service busybee metricark_promql sum%28kube_node_status_condition%7Bcondition%3D%22Ready%22%2Cstatus%21%3D%22true%22%7D%29 http%3A%2F%2Fnc-prometheus-server.nexclipper.svc.cluster.local` 
-GET_JSON=`ssh provbee-service busybee $GET_CMD $GET_QL $GET_ENDPOINT` 
+GET_JSON=`ssh provbee-service busybee $GET_CMD $GET_PROMQL $GET_ENDPOINT` 
 
 ##### INFO
 #TASK_RESULT=$(echo "$GET_JSON")
