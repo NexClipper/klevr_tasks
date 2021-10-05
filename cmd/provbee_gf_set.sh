b0fe76ebef9ad9d9d88bbc87132006e752a40f74
joel83 <75293076+joel-nexcl@users.noreply.github.com>
2021-10-05 15:19:46 +0900

    Create provbee_gf_set.sh

---
+++ cmd/provbee_gf_set.sh
@@ -0,0 +1,9 @@
+JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
+####################################################################
+GET_NS=$(echo ${JSON_TASK_PARAMS} | jq -r '.gf_namespace')
+GET_GF_JSON=$(echo ${JSON_TASK_PARAMS} | jq -r '.gf_setjson')
+
+GET_JSON=$(ssh provbee-service busybee grafana $GET_NS $GET_GF_JSON )
+
+##### INFO
+TASK_RESULT=$(echo "$GET_JSON")
