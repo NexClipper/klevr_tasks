2663adee9ef25c6b620d40097de194e31985516e
SKYul <folimworld@gmail.com>
2021-10-05 18:02:08 +0900

    Update provbee_gf_set.sh

--- cmd/provbee_gf_set.sh
+++ cmd/provbee_gf_set.sh
@@ -3,7 +3,7 @@ JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 GET_NS=$(echo ${JSON_TASK_PARAMS} | jq -r '.gf_namespace')
 GET_GF_JSON=$(echo ${JSON_TASK_PARAMS} | jq -r '.gf_setjson')
 
-GET_JSON=$(ssh provbee-service busybee grafana $GET_NS $GET_GF_JSON )
+GET_JSON=$(ssh provbee-service busybee grafana api $GET_NS $GET_GF_JSON )
 
 ##### INFO
 TASK_RESULT=$(echo "$GET_JSON")
