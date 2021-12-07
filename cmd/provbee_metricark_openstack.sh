106bde4cc1c2f4343029e68fad70b061f88af803
joel83 <75293076+joel-nexcl@users.noreply.github.com>
2021-12-07 11:31:18 +0900

    Update provbee_metricark_openstack.sh

--- cmd/provbee_metricark_openstack.sh
+++ cmd/provbee_metricark_openstack.sh
@@ -1,15 +1,15 @@
 ##### JSON
 JSON_TASK_PARAMS=${TASK_ORIGIN_PARAM}
 GET_CMD=$(echo ${JSON_TASK_PARAMS} | jq -r '.metricark_cmd')
-GET_PROMQL=$(echo ${JSON_TASK_PARAMS} | jq -r '.metricark_promql')
-GET_ENDPOINT=$(echo ${JSON_TASK_PARAMS} | jq -r '.metricark_endpoint')
+GET_PROJNAME=$(echo ${JSON_TASK_PARAMS} | jq -r '.projectName')
+GET_DOMAINID=$(echo ${JSON_TASK_PARAMS} | jq -r '.domainId')
 
 
 #TEST
-if [ "$GET_CMD" = "" ]; then GET_CMD="metricark_promql"; fi
+if [ "$GET_CMD" = "" ]; then GET_CMD="metricark_openstack_nodes"; fi
 ##### RUN
-#GET_JSON=`ssh provbee-service busybee metricark_promql sum%28kube_node_status_condition%7Bcondition%3D%22Ready%22%2Cstatus%21%3D%22true%22%7D%29 http%3A%2F%2Fnc-prometheus-server.nexclipper.svc.cluster.local` 
-GET_JSON=`ssh provbee-service busybee $GET_CMD $GET_PROMQL $GET_ENDPOINT` 
+#GET_JSON=`ssh provbee-service busybee metricark_openstack_nodes pName dId` 
+GET_JSON=`ssh provbee-service busybee $GET_CMD $GET_PROJNAME $GET_DOMAINID` 
 
 ##### INFO
 #TASK_RESULT=$(echo "$GET_JSON")
